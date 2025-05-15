00b5346c9ebf9a6c493768761968d4456e325e34
Kirill <kirilldevelop1511@mail.ru>
2025-05-15 21:47:25 +0300

    Исправления

--- dz-3.playground/Contents.swift
+++ dz-3.playground/Contents.swift
@@ -16,6 +16,8 @@ if count % 100 >= 11 && count % 100 <= 14 || count % 10 >= 5 {
     apples = "яблоко"
 } else if count % 10 >= 2 && count % 10 <= 4 {
     apples = "яблока"
+} else {
+    apples = "яблок"
 }
 /*
  Пример результата
